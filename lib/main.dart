0e7ccc41b190cdd54eff04eea8244d1ed1d6c30f
ssma100701 <ssma100701@gmail.com>
2022-11-16 14:36:41 +0800

    xylophone init

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp());
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Container(),
+        ),
+      ),
+    );
+  }
 }
