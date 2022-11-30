4f8ff0d6f069ab6d5e41eacf3c1ce92cc909deaa
ssma100701 <ssma100701@gmail.com>
2022-11-30 23:09:13 +0800

    clima init

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
+import 'screens/loading_screen.dart';
 
-void main() {
-  runApp(MaterialApp());
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: LoadingScreen(),
+    );
+  }
 }
