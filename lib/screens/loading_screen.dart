2fd3c4b82b7287afab604680d32ed7a2499d5b56
ssma100701 <ssma100701@gmail.com>
2023-01-12 18:12:45 +0800

    location

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,6 +7,19 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+  void getLocation() async{
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.longitude);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
