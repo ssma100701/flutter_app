bed86c7f7b2d5714e4788f98528164c26ba66eb3
ssma100701 <ssma100701@gmail.com>
2023-02-13 16:52:43 +0800

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,28 @@
 import 'package:flutter/material.dart';
+import '/screens/welcome_screen.dart';
+import '/screens/login_screen.dart';
+import '/screens/registration_screen.dart';
+import '/screens/chat_screen.dart';
 
-void main() {
-  runApp(MaterialApp());
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          bodyLarge: TextStyle(color: Colors.black),
+        ),
+      ),
+      home: WelcomeScreen(),
+      initialRoute: '/registration',
+      routes: {
+        '/welcome': (context) => WelcomeScreen(),
+        '/login': (context) => LoginScreen(),
+        '/registration': (context) => RegistrationScreen(),
+        '/chat': (context) => ChatScreen()
+      },
+    );
+  }
 }
